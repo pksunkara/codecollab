8fef614048284c6871b102ef545eb76947c6ce2a
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2011-09-18 07:46:10 +0530

    Proper jquery

--- public/javascripts/collab.js
+++ public/javascripts/collab.js
@@ -2,7 +2,7 @@ var socket = io.connect(window.location.origin);
 var members = [];
 
 updateMembers = function() {
-  var e = document.getElementsByClassName('members')[0];
+  var e = $('.members')[0];
   var r = members[0];
   for(i=1;i<members.length;i++)
     r += "\n"+members[i];
@@ -10,12 +10,12 @@ updateMembers = function() {
 }
 
 putChat = function(msg) {
-  var e = document.getElementsByClassName('chatbox')[0];
+  var e = $('.chatbox')[0];
   e.innerHTML = e.innerHTML+msg+"\n";
 }
 
 sendChat = function() {
-  var e = document.getElementsByClassName('chat')[0];
+  var e = $('.chat')[0];
   socket.emit('chat', e.value);
   e.value = '';
 }
