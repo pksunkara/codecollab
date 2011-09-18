4ca89fcfee00b2b679d754e2e4b9ac28cbf25780
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2011-09-18 11:40:54 +0530

    Revert "minor change"
    This reverts commit a1069634312896a00160c5dba44b151deade0cc1.

--- public/javascripts/collab.js
+++ public/javascripts/collab.js
@@ -47,9 +47,9 @@ key_handler = function (event) {
 
 socket.on('edit', function (data){
   var text = acee.getSession().getValue();
-  //text = text.substr(0,data.cursor-1) + data.edit + text.substr(data.cursor-1+data.edit.length);
+  text = text.substr(0,data.cursor-1) + data.edit + text.substr(data.cursor-1+data.edit.length);
   docVersion += 1;
-  acee.getSession().insert(data.edit);
+  acee.getSession().setValue(text);
 });
 
 socket.on('version', function(data){
